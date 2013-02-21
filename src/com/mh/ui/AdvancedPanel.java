798ce2f3ccff3e6805879981ad0334454c2cc78a
Tom <tomsebastan@gmail.com>
2013-02-21 13:37:21 +0530

    add files for backup

---
+++ src/com/mh/ui/AdvancedPanel.java
@@ -0,0 +1,15 @@
+package com.mh.ui;
+
+import javax.swing.JPanel;
+
+import com.Main;
+
+public class AdvancedPanel extends JPanel{
+	
+	private Main main;
+
+	public AdvancedPanel(Main main){
+		this.main =main;
+	}
+
+}
